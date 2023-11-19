5450ed09d276ef60ebdf30d01a87c8a37964a7ac
Seunghee <99533453+mowgood@users.noreply.github.com>
2023-11-19 17:15:02 +0900

    feat: product KafkaConsumer 수량 업데이트 (#23)

---
+++ product-service/src/main/java/com/example/productservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,41 @@
+package com.example.productservice.messagequeue;
+
+import com.example.productservice.domain.Product;
+import com.example.productservice.repository.ProductRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaConsumer {
+    private final ProductRepository productRepository;
+
+    @KafkaListener(topics = "product-qty-topic")
+    public void updateProductStock(String kafkaMessage) {
+        log.info("Kafka Message ->" + kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
+            });
+        } catch (JsonProcessingException exception) {
+            exception.printStackTrace();
+        }
+
+        Product product = productRepository.findByProductId((String) map.get("productId"));
+        if (product == null) {
+
+        }
+        product.updateStock((Integer) map.get("quantity"));
+    }
+}
