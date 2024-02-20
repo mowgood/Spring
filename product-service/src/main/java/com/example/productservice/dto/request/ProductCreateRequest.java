a2ac6b234d7753e2b593aeac54d15c207462ae1d
mowgood <woforky@gmail.com>
2024-02-21 08:58:44 +0900

    feat: [Product] 상품 생성 request 필드 값 검증 기능 추가

--- product-service/src/main/java/com/example/productservice/dto/request/ProductCreateRequest.java
+++ product-service/src/main/java/com/example/productservice/dto/request/ProductCreateRequest.java
@@ -1,18 +1,23 @@
 package com.example.productservice.dto.request;
 
 import com.example.productservice.enumeration.Category;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class ProductCreateRequest {
 
+    @NotBlank(message = "상품 이름을 입력하세요.")
     private String productName;
-
+    
     private Category category;
 
+    @NotNull(message = "상품 수량을 입력하세요.")
     private int stock;
 
+    @NotNull(message = "상품 가격을 입력하세요.")
     private int unitPrice;
 
     @Builder
