5672815ae3bc224594d3acf06102250f3ceac034
mowgood <woforky@gmail.com>
2024-02-21 08:51:21 +0900

    fix: [Product] 음식 카테고리 Enum에 value값 추가

--- product-service/src/main/java/com/example/productservice/enumeration/Category.java
+++ product-service/src/main/java/com/example/productservice/enumeration/Category.java
@@ -1,5 +1,14 @@
 package com.example.productservice.enumeration;
 
 public enum Category {
-    FOOD, CLOTHES, FURNITURE, COSMETIC
+    FOOD("식품"),
+    CLOTHES("의류"),
+    FURNITURE("가구"),
+    COSMETIC("화장품");
+
+    private String value;
+
+    Category(String value) {
+        this.value = value;
+    }
 }
