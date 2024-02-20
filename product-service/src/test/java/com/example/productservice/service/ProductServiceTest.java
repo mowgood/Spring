327d7c0428544a5b043cfb5c6f94ea281bf6ea44
mowgood <woforky@gmail.com>
2024-02-21 08:53:38 +0900

    test: [Product] 상품 생성 기능 테스트 코드 추가

--- product-service/src/test/java/com/example/productservice/service/ProductServiceTest.java
+++ product-service/src/test/java/com/example/productservice/service/ProductServiceTest.java
@@ -1,11 +1,15 @@
 package com.example.productservice.service;
 
 import com.example.productservice.domain.Product;
+import com.example.productservice.dto.request.ProductCreateRequest;
+import com.example.productservice.dto.response.ProductCreateResponse;
 import com.example.productservice.enumeration.Category;
 import com.example.productservice.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -36,5 +40,26 @@ class ProductServiceTest {
 
         // then
         assertEquals(product.getStock(), stock);
+    }
+
+    @Test
+    void 상품_생성_성공() throws Exception {
+        // given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .productName("햄버거")
+                .category(Category.FOOD)
+                .stock(110)
+                .unitPrice(9000)
+                .build();
+
+        // when
+        ProductCreateResponse response = productService.createProduct(request);
+        Optional<Product> product = productRepository.findById(response.getSavedId());
+
+        // then
+        assertEquals(request.getProductName(), product.get().getProductName());
+        assertEquals(request.getCategory(), product.get().getCategory());
+        assertEquals(request.getStock(), product.get().getStock());
+        assertEquals(request.getUnitPrice(), product.get().getUnitPrice());
     }
 }
